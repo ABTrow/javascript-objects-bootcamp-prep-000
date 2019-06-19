c2a39690bec4209ddf2a7b9a322cfe96af33121e
Austin Trow <austintrow@gmail.com>
2019-06-19 18:22:12 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,11 @@
-var playlist = {}
+var playlist = {beck: "100 BPM", lorde: "Royals", chvrches: "some song I don't remember"};
+
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist[artist];
+  return playlist;
+}
